bf5313e2c1e5f01c969d3ad0721076082c056a42
Maria Nery <67118326+mnvnery@users.noreply.github.com>
2022-12-05 09:32:38 +0000

    content

---
+++ data/info.js
@@ -0,0 +1,10 @@
+const scanLinks = [
+    { href: 'https://www.instagram.com/ar/1178928943035721/?ch=NGYxYjg1Y2VlM2JmNjQwM2I0NjI5ZTQxMTE4ODVhOTc%3D', slug: 'scan1', collected: true },
+    { href: 'https://www.instagram.com/ar/1178928943035721/?ch=NGYxYjg1Y2VlM2JmNjQwM2I0NjI5ZTQxMTE4ODVhOTc%3D', slug: 'scan2', collected: false },
+    { href: 'https://www.instagram.com/ar/1178928943035721/?ch=NGYxYjg1Y2VlM2JmNjQwM2I0NjI5ZTQxMTE4ODVhOTc%3D', slug: 'scan3', collected: false  },
+    { href: 'https://www.instagram.com/ar/1178928943035721/?ch=NGYxYjg1Y2VlM2JmNjQwM2I0NjI5ZTQxMTE4ODVhOTc%3D', slug: 'scan4', collected: true  },
+    { href: 'https://www.instagram.com/ar/1178928943035721/?ch=NGYxYjg1Y2VlM2JmNjQwM2I0NjI5ZTQxMTE4ODVhOTc%3D', slug: 'scan5', collected: false },
+    { href: 'https://www.instagram.com/ar/1178928943035721/?ch=NGYxYjg1Y2VlM2JmNjQwM2I0NjI5ZTQxMTE4ODVhOTc%3D', slug: 'scan6', collected: false  },
+]
+
+export default scanLinks
